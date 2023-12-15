dc5cef9a28e4ef62d91c6a44f22c8d0902944113
Virginie Dourson <virginiedourson@gmail.com>
2023-12-15 16:50:10 +0100

    init week4

---
+++ helloWorld/src/test/java/vd/vdourson/week4/helloWorld/AppTest.java
@@ -0,0 +1,20 @@
+package vd.vdourson.week4.helloWorld;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
