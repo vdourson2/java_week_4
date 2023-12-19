83f0411f4442825da715bfa046568ad14181bfad
Virginie Dourson <virginiedourson@gmail.com>
2023-12-19 11:51:47 +0100

    Challenge1

--- helloWorld/src/main/java/vd/vdourson/week4/helloWorld/App.java
+++ helloWorld/src/main/java/vd/vdourson/week4/helloWorld/App.java
@@ -1,13 +1,16 @@
 package vd.vdourson.week4.helloWorld;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import vd.vdourson.week4.helloWorld.service.HelloWorldService;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+    HelloWorldService helloWorld = context.getBean(HelloWorldService.class);
+    System.out.println(helloWorld.hello());
     }
 }
---
+++ helloWorld/src/main/java/vd/vdourson/week4/helloWorld/service/HelloWorldService.java
@@ -0,0 +1,40 @@
+package vd.vdourson.week4.helloWorld.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Service;
+
+@Service
+@PropertySource("classpath:application.properties")
+public class HelloWorldService implements EnvironmentAware {
+	
+	@Value("${configured_language}")
+	private String language;
+	@Autowired
+	private Environment environment;
+	
+	public String hello() {
+		String property = this.language + "_hello_world";
+		String msg = this.environment.getProperty(property);
+		return msg;
+	}
+
+	public String getLanguage() {
+		return language;
+	}
+
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+
+	public Environment getEnvironment() {
+		return environment;
+	}
+
+	public void setEnvironment(Environment environment) {
+		this.environment = environment;
+	}	
+}
